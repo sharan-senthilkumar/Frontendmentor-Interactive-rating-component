cf6faa61feca907374dce78c5d028b6b29335447
Sharan SG <sgsharan89@gmail.com>
2024-02-27 14:43:16 +0530

    please

--- app.py
+++ app.py
@@ -22,5 +22,4 @@ def thank_you():
     global global_rating
     return render_template('result.html', rating=global_rating)
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+
